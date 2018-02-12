812a3b9acd2c62ae63da2732bede24ccb6e8d17e
xdqc <qichen.d@outlook.com>
2018-02-13 01:03:58 +1300

    cipher

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,16 @@
-def rotate(text, key):
-    pass
+import re
+
+
+def cipher(c='', k=0):
+    if c.islower():
+        return chr((ord(c) + k - ord('a')) % 26 + ord('a'))
+    elif c.isupper():
+        return chr(ord(c) + k) if ord(c) + k <= ord('Z') else chr(ord(c) + k - 26)
+    else:
+        return c
+
+
+def rotate(text='', key=0):
+    re.sub('\s+', ' ', text)
+    text = map(lambda c: cipher(c, key), text)
+    return ''.join(list(text))
